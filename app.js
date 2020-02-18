c285aea24a7dbef0ca6ae43c2f2f32a12ec849ab
joeyzhou98 <joeyzhou7002@gmail.com>
2020-02-18 02:00:30 -0500

    using fuzzy search for string comparing

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fuzzysort = require('fuzzysort')
+const fs = require('fs');
 const port = 3000;
 
-app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/index.html')))
+const rawdata = fs.readFileSync('games.json');
+const data = JSON.parse(rawdata);
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+app.get('/search', function(req, res) {
+    const query = req.query.query;
+    res.send(fuzzysort.go(query, data, {
+        key: 'title',
+        limit: 10
+    }));
+});
+
+app.get('/', (req, res) =>  res.sendFile(path.join(__dirname + '/index.html')));
+
+app.listen(port, () => console.log('Server listening on port ' + port));
